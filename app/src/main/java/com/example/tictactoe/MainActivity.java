a899db1201c6d3df12f14efe8f594c24c9180f5b
Alexandra Haley <72041575+Tinet0624@users.noreply.github.com>
2021-10-27 12:05:54 -0700

    refactored some of the switch

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -7,16 +7,10 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-
-// Ok this is messy and redundant and need of refactoring soooo bad! I remembered using switches in Joes class so tried it here.
-// Im not happy with the switch and all the breaks but it took me so long to remember how the switch worked and to get it working here.
-// need to change the text so that it is in the strings.xml file
-// need to figure out putting buttons in an array and pulling variables dynamically instead of hardcoded the way they are now...
-// look forward to this changing for the next version and for clean up as I go. This is the messy version!
-
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private boolean playerXTurn;
+    private String emptyString;
     private TextView playerText;
     private Button button1;
     private Button button2;
@@ -36,6 +30,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         // set up player turn variable
         playerXTurn = true;
+
+        // set up variable for empty string
+        emptyString = "";
 
         // set up buttons and text field
 
@@ -67,16 +64,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private void clearButton() {
 
-        playerText.setText("Player X's Turn");
-        button1.setText("");
-        button2.setText("");
-        button3.setText("");
-        button4.setText("");
-        button5.setText("");
-        button6.setText("");
-        button7.setText("");
-        button8.setText("");
-        button9.setText("");
+        playerText.setText(R.string.xPLayer);
+        button1.setText(emptyString);
+        button2.setText(emptyString);
+        button3.setText(emptyString);
+        button4.setText(emptyString);
+        button5.setText(emptyString);
+        button6.setText(emptyString);
+        button7.setText(emptyString);
+        button8.setText(emptyString);
+        button9.setText(emptyString);
         playerXTurn = true;
     }
 
@@ -84,118 +81,107 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         clearButton();
     }
 
+    public void playerX(){
+        playerText.setText(R.string.xPLayer);
+        playerXTurn = true;
+    }
+    public void playerO(){
+        playerText.setText(R.string.oPLayer);
+        playerXTurn = false;
+    }
+
     @Override
     public void onClick(View view) {
 
-        String emptyString = "";
-
         switch (view.getId()){
             case R.id.button1:
-                if(emptyString.equals(button1.getText()) && playerXTurn){
-                    button1.setText("X");
-                    playerText.setText("Player O's Turn");
-                    playerXTurn = false;
+                if(emptyString.contentEquals(button1.getText()) && playerXTurn){
+                    button1.setText(R.string.X);
+                    playerO();
                 }
                 if(emptyString.equals(button1.getText()) && !playerXTurn){
-                    button1.setText("O");
-                    playerText.setText("Player X's Turn");
-                    playerXTurn = true;
+                    button1.setText(R.string.O);
+                    playerX();
                 }
                 break;
             case R.id.button2:
                 if(emptyString.equals(button2.getText()) && playerXTurn){
-                    button2.setText("X");
-                    playerText.setText("Player O's Turn");
-                    playerXTurn = false;
+                    button2.setText(R.string.X);
+                    playerO();
                 }
                 if(emptyString.equals(button2.getText()) && !playerXTurn){
-                    button2.setText("O");
-                    playerText.setText("Player X's Turn");
-                    playerXTurn = true;
+                    button2.setText(R.string.O);
+                    playerX();
                 }
                 break;
             case R.id.button3:
                 if(emptyString.equals(button3.getText()) && playerXTurn){
-                    button3.setText("X");
-                    playerText.setText("Player O's Turn");
-                    playerXTurn = false;
+                    button3.setText(R.string.X);
+                    playerO();
                 }
                 if(emptyString.equals(button3.getText()) && !playerXTurn){
-                    button3.setText("O");
-                    playerText.setText("Player X's Turn");
-                    playerXTurn = true;
+                    button3.setText(R.string.O);
+                    playerX();
                 }
                 break;
             case R.id.button4:
                 if(emptyString.equals(button4.getText()) && playerXTurn){
-                    button4.setText("X");
-                    playerText.setText("Player O's Turn");
-                    playerXTurn = false;
+                    button4.setText(R.string.X);
+                    playerO();
                 }
                 if(emptyString.equals(button4.getText()) && !playerXTurn){
-                    button4.setText("O");
-                    playerText.setText("Player X's Turn");
-                    playerXTurn = true;
+                    button4.setText(R.string.O);
+                    playerX();
                 }
                 break;
             case R.id.button5:
                 if(emptyString.equals(button5.getText()) && playerXTurn){
-                    button5.setText("X");
-                    playerText.setText("Player O's Turn");
-                    playerXTurn = false;
+                    button5.setText(R.string.X);
+                    playerO();
                 }
                 if(emptyString.equals(button5.getText()) && !playerXTurn){
-                    button5.setText("O");
-                    playerText.setText("Player X's Turn");
-                    playerXTurn = true;
+                    button5.setText(R.string.O);
+                    playerX();
                 }
                 break;
             case R.id.button6:
                 if(emptyString.equals(button6.getText()) && playerXTurn){
-                    button6.setText("X");
-                    playerText.setText("Player O's Turn");
-                    playerXTurn = false;
+                    button6.setText(R.string.X);
+                    playerO();
                 }
                 if(emptyString.equals(button6.getText()) && !playerXTurn){
-                    button6.setText("O");
-                    playerText.setText("Player X's Turn");
-                    playerXTurn = true;
+                    button6.setText(R.string.O);
+                    playerX();
                 }
                 break;
             case R.id.button7:
                 if(emptyString.equals(button7.getText()) && playerXTurn){
-                    button7.setText("X");
-                    playerText.setText("Player O's Turn");
-                    playerXTurn = false;
+                    button7.setText(R.string.X);
+                    playerO();
                 }
                 if(emptyString.equals(button7.getText()) && !playerXTurn){
-                    button7.setText("O");
-                    playerText.setText("Player X's Turn");
-                    playerXTurn = true;
+                    button7.setText(R.string.O);
+                    playerX();
                 }
                 break;
             case R.id.button8:
                 if(emptyString.equals(button8.getText()) && playerXTurn){
-                    button8.setText("X");
-                    playerText.setText("Player O's Turn");
-                    playerXTurn = false;
+                    button8.setText(R.string.X);
+                    playerO();
                 }
                 if(emptyString.equals(button8.getText()) && !playerXTurn){
-                    button8.setText("O");
-                    playerText.setText("Player X's Turn");
-                    playerXTurn = true;
+                    button8.setText(R.string.O);
+                    playerX();
                 }
                 break;
             case R.id.button9:
                 if(emptyString.equals(button9.getText()) && playerXTurn){
-                    button9.setText("X");
-                    playerText.setText("Player O's Turn");
-                    playerXTurn = false;
+                    button9.setText(R.string.X);
+                    playerO();
                 }
                 if(emptyString.equals(button9.getText()) && !playerXTurn){
-                    button9.setText("O");
-                    playerText.setText("Player X's Turn");
-                    playerXTurn = true;
+                    button9.setText(R.string.O);
+                    playerX();
                 }
                 break;
         }
